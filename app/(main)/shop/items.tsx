4237fa5b45caba13cd852640c23c7f9cbeae0fcc
1370qqcomddd <105094143+1370qqcomddd@users.noreply.github.com>
2024-11-28 08:07:59 +0800

    无限橙子

--- app/(main)/shop/items.tsx
+++ app/(main)/shop/items.tsx
@@ -79,7 +79,7 @@ export const Items = ({
 
         <div className="flex-1">
           <p className="text-base font-bold text-neutral-700 lg:text-xl">
-            无限橙子
+           Hippo+ (无限橙子)
           </p>
         </div>
 
